1cef40c9a92933a13472567708e38ec5e24d1db6
Zeke Gan <traveller870720@gmail.com>
2026-05-11 17:22:06 +0900

    feat: initialize electron shell.

---
+++ electron/main.ts
@@ -0,0 +1,23 @@
+import { BrowserWindow, app } from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.ts'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
